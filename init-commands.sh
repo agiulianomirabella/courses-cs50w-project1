2907553ec3ebc3eb2ba86fa2a4443e8a44e93376
gmirabella <giulianomirabella@gmail.com>
2020-05-21 12:58:56 +0200

    Update init-commands.sh

---
+++ init-commands.sh
@@ -0,0 +1,4 @@
+cd /home/giuliano/Desktop/courses-cs50w-project1
+export FLASK_APP=application.py 
+export FLASK_DEBUG=1 
+export DATABASE_URL=postgres://wfwoewdbugkurk:85896f0531941d15a43b9b864b47376882347a56b7d19161fb044850141b8ebe@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/d2ldkrsktl45l4
